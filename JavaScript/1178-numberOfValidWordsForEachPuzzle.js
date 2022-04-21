13f3e00ddde321ef3611c7656f1e9e86dfb681b5
Mytruc Nguyen <80004058+MytrucNguyen@users.noreply.github.com>
2022-04-20 17:43:16 -0700

    Leet Code Problem

---
+++ JavaScript/1178-numberOfValidWordsForEachPuzzle.js
@@ -0,0 +1,10 @@
+var findNumOfValidWords = function(words, puzzles) {
+    let count = 0;
+    const firstLetter = puzzles.map(word => word[0])
+
+    
+
+}
+  
+findNumOfValidWords(["apple","pleas","please"], ["aelwxyz","aelpxyz","aelpsxy","saelpxy","xaelpsy"])
+  
---
+++ JavaScript/94-BinaryTreeInorderTraversal.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+ var inorderTraversal = function(root) {
+    let output = []
+    
+    InorderTraversal(root) 
+    return output
+
+    function InorderTraversal(root){
+        if(root == null){
+            return;
+        }
+        
+        InorderTraversal(root.left);
+        output.push(root.val)        
+        InorderTraversal(root.right)
+    }
+
+    
+};
