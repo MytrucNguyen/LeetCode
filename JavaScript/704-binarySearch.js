1b746c5a879c68491ea29c123671a022e34a3618
Mytruc Nguyen <80004058+MytrucNguyen@users.noreply.github.com>
2022-05-01 15:02:33 -0700

    LeetCode Problem 704

--- JavaScript/704-binarySearch.js
+++ JavaScript/704-binarySearch.js
@@ -4,5 +4,21 @@
  * @return {number}
  */
  var search = function(nums, target) {
+    let start = 0;
+    let end = nums.length - 1;
     
+    while (start <= end){
+        let midpoint = Math.floor((start + end) / 2)
+        
+        if(nums[midpoint] === target) {
+            return midpoint;
+        }
+        else if (nums[midpoint] < target){
+            start++
+        }
+        else {
+            end--
+        }
+    }
+    return -1
 };
