e95a4296bf01f3edf80af3ecea91d96f82054bcf
Mytruc Nguyen <80004058+MytrucNguyen@users.noreply.github.com>
2022-05-10 20:46:56 -0700

    344. Reverse String

---
+++ JavaScript/344-reverseString.js
@@ -0,0 +1,9 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+/* Reversing the string. */
+ var reverseString = function(s) {
+    return s.reverse()
+};
+console.log(reverseString(["h","e","l","l","o"]))
