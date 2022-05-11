f746d5b8b62a94e7487f6580a1b7e1f1056a9227
Mytruc Nguyen <80004058+MytrucNguyen@users.noreply.github.com>
2022-05-10 21:17:39 -0700

    11. Container With Most Water

---
+++ JavaScript/11-containerWithMostWater.js
@@ -0,0 +1,30 @@
+/**
+ * We start at the beginning and end of the array and find the minimum value between the two. 
+ * We then multiply that value by the difference between the two indexes. 
+ * We then move the index with the smaller value towards the center of the array. 
+ * We repeat this process until the two indexes meet.
+ * @param height - [1,8,6,2,5,4,8,3,7]
+ * @returns 49
+ */
+const maxArea = (height) => {
+    let maxArea = 0,
+        startOfIndex = 0,
+        endOfIndex = height.length - 1
+
+    while (startOfIndex < endOfIndex) {
+        /* Finding the minimum value between the two indexes. */
+        let minArea = Math.min(height[startOfIndex], height[endOfIndex]);
+
+        /* Finding the maximum area between the two indexes. */
+        maxArea = Math.max(maxArea, minArea * (endOfIndex - startOfIndex));
+
+        /* Moving the index with the smaller value towards the center of the array. */
+        if (height[startOfIndex] < height[endOfIndex]) {
+            startOfIndex++;
+        } else {
+            endOfIndex--;
+        }
+    }
+    return maxArea
+}
+console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]))
